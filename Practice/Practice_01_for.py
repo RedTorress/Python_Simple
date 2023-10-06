322d29cd174a41f6b62342d76281fe0679681f38
RedTorress <143864467+RedTorress@users.noreply.github.com>
2023-10-06 12:45:39 +0900

    Add Practice_01_for.py

---
+++ Practice/Practice_01_for.py
@@ -0,0 +1,27 @@
+# 문제1) 사용자가 입력한 단수를 출력하는 코드
+# dan = int(input("단수: "))
+# for i in range(1, 10):
+#     print(f"{dan}*{i}={dan * i}") # 전체주석 -> 컨트롤 /
+
+
+# 문제2) 2단부터 9단까지 출력(중첩 for문)
+for i in range(2,10):
+    for j in range(1,10):
+        print(f"{i}*{j}={i*j}")
+
+# 문제3) list a 의 평균값을 계산하세요.
+a = [1, 2, 3, 4, 5, 99, 87, 54, 2, 5, 4]
+total = 0
+for num in a:
+    total += num # total = total + num
+# a 길이 => len(a)
+result = total / len(a
+
+
+#round(값, 자리수) : 자리수만큼 반올림
+print(round(result, 2)) # 평균값 출력
+
+# 문제4) list b 에서 최소값 찾기
+b = [22, 1, 4, 7, 98]
+format()
+print(num_min) # 1 출력
