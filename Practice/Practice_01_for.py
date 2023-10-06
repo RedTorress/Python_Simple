a78be838b526ee6147f866b5df48551b953f3655
RedTorress <143864467+RedTorress@users.noreply.github.com>
2023-10-06 13:03:40 +0900

    Update Practice_01_for.py

--- Practice/Practice_01_for.py
+++ Practice/Practice_01_for.py
@@ -15,7 +15,7 @@ total = 0
 for num in a:
     total += num # total = total + num
 # a 길이 => len(a)
-result = total / len(a
+result = total / len(a)
 
 
 #round(값, 자리수) : 자리수만큼 반올림
@@ -23,5 +23,10 @@ print(round(result, 2)) # 평균값 출력
 
 # 문제4) list b 에서 최소값 찾기
 b = [22, 1, 4, 7, 98]
-format()
+n = len(b)
+for i in range(n):
+    for j in range(n-i-1):
+        if(b[j]>b[j+1]):
+             b[j],b[j+1] = b[j+1], b[j]
+num_min = b[0]
 print(num_min) # 1 출력
