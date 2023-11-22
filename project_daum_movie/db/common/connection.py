e0f4e4af7ed79ad9df7289f3728b17d8f9fbee2d
TheKoreanGuy <143864467+TheKoreanGuy@users.noreply.github.com>
2023-11-22 17:26:56 +0900

    Update db

--- project_daum_movie/db/common/connection.py
+++ project_daum_movie/db/common/connection.py
@@ -16,7 +16,7 @@ import pymysql
 def connection():
     try:
         conn = pymysql.connect(
-            hosy="127.0.0.1",
+            host="127.0.0.1",
             port=3306,
             user="root",
             password="12341234",
